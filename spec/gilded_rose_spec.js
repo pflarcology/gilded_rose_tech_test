91f6198cdd79d7ac76fa512b0dcb86e02e51472b
pflarcology <rhrg500@gmail.com>
2017-01-25 10:15:25 +0000

    adds test for Aged Brie

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -3,7 +3,13 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     items = [ new Item("foo", 0, 0) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
+  });
+
+  it("'Aged Brie' items should increase in quality", function() {
+    items = [ new Item("Aged Brie", 0, 0) ];
+    update_quality();
+    expect(items[0].quality).toEqual(2)
   });
 
 });
